14c203847224d1393907012b452d30784bf9881e
HSR <hectorsilvarobles@gmail.com>
2019-07-10 07:31:09 -0700

    nodemailer

--- server/index.js
+++ server/index.js
@@ -7,12 +7,35 @@ app.use(express.json())
 
 const {EMAIL, EMAIL_PASSWORD} = process.env
 
+
 app.post('/api/send', (req, res, next) => {
+    console.log(req.body)
     const {name, email, title, message} = req.body
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
+            user: EMAIL,
+            pass: EMAIL_PASSWORD
+        }
+    });
 
+    const mailOptions = {
+        from: `${EMAIL}`,
+        to: `${email}`,
+        subject: `${title} by ${name}`,
+        text: `${message}`,
+        replyTo: ''
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err){
+            console.error('there was an error ', err)
+        } else {
+            console.log('here is the email ', info)
         }
     })
-})
+});
+
+const port = 4000;
+
+app.listen(port, ()=> console.log(`Listening on port ${port}`))
