a920d4b2c4d72f8742e0e7a6c6f732aa12c8dcd7
HSR <hectorsilvarobles@gmail.com>
2019-07-14 13:42:22 -0700

    logo

--- src/Components/banner/Banner.js
+++ src/Components/banner/Banner.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './banner.css'
+import $ from 'jquery'
 
-export default function Banner() {
-    return (
-        <div id='banner'>
-            <h1>Banner</h1>
-        </div>
-    )
+export default class Banner extends React.Component {
+    
+    componentDidMount(){
+        $(window).scroll(function() {
+            var scrollPos = $(this).scrollTop();
+            console.log(scrollPos)
+            $('.hero-back').css({
+                'background-size' : 200 + scrollPos + 'vh'
+            })
+        })
+    }
+    render(){
+        return (
+            <div id='banner'>
+                {/* <div className='hero-back'>
+                    <img src='http://pngriver.com/wp-content/uploads/2018/04/Download-Mountain-Transparent-Background.png' />
+                </div> */}
+                <div id='main'>
+                    <img src='./hsr.png' />
+                </div>
+            </div>
+        )
+    }
 }
